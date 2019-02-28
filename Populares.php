955ceb9eda645b16a5076a6ca2509b62415980e3
LonelyWolf13 <alondigu@gmail.com>
2019-02-27 23:00:25 -0600

    Ligero retrazo

--- Populares.php
+++ Populares.php
@@ -1,14 +1,23 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+    <link rel="stylesheet" href="styles/populares.css">
     <meta charset="utf-8">
     <title></title>
   </head>
   <body>
     <?php
 
-    require_once("sections/");
+    require_once("sections/encabezado.php");
 
      ?>
+
+
+
+     <?php
+
+     require_once("sections/pie.php");
+
+      ?>
   </body>
 </html>
